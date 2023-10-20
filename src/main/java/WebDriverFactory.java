1099cbfb7edaa593e58341cf4588b3cad8cc9ec9
belova <belova@geobuilder.ru>
2023-10-20 14:57:48 +0300

    add pass log

---
+++ src/main/java/WebDriverFactory.java
@@ -0,0 +1,5 @@
+public class WebDriverFactory {
+    public static create(){
+
+    }
+}
--- src/test/java/LogInOTUS.java
+++ src/test/java/LogInOTUS.java
@@ -11,11 +11,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Properties;
 
 public class LogInOTUS {
     private WebDriver driver;
+    Properties property = new Properties(System.getProperties());
 
-
+    private final String base_url = property.getProperty("base.url");
+    private final String email = System.getProperty("login");
+    private final String password = System.getProperty("pwd");
     @BeforeEach
 //    Открыть Chrome
     public void openBrouser() {
@@ -24,19 +28,25 @@ public class LogInOTUS {
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
     }
+    @AfterEach
+    public void closeBrouser(){
+        if(this.driver != null) {
+            this.driver.close();
+            this.driver.quit();
+        }
 
     @Test
     public void loginOtus() {
 //   1. Открыть https://otus.ru
-        driver.get("https://otus.ru");
+        driver.get(base_url);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 //   2. Авторизоваться на сайте
         driver.findElement(By.cssSelector(".header3__button-sign-in")).click();//нажать кнопку войти
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath
                 ("//*[contains(text(), 'Войдите в свой аккаунт')]")));//проверить, что открылось окно ввода логина/пароля
         //ввести логин пароль
-        driver.findElement(By.cssSelector("input.js-email-input[placeholder='Электронная почта']")).sendKeys("dafome4086@aicogz.com");//login
-        driver.findElement(By.cssSelector("input.js-psw-input[placeholder='Введите пароль']")).sendKeys("OtusTest12#");//pwd
+        driver.findElement(By.cssSelector("input.js-email-input[placeholder='Электронная почта']")).sendKeys(email);//login "dafome4086@aicogz.com"
+        driver.findElement(By.cssSelector("input.js-psw-input[placeholder='Введите пароль']")).sendKeys(password);//pwd "OtusTest12#"
         driver.findElement(By.cssSelector(".new-input-line_relative>button.new-button_md")).click();//нажать войти
         //проверяем, что вход успешный
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".header3__user-info-name")));
@@ -50,9 +60,6 @@ public class LogInOTUS {
 //    Проверить, что в разделе "О себе" отображаются указанные ранее данные
     }
 
-    @AfterEach
-    public void closeBrouser(){
-        driver.close();
-        driver.quit();
+
     }
 }
