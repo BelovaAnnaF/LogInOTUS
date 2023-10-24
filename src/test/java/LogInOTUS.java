77351e535eee89bdedce0494621ff9e968d66682
belova <belova@geobuilder.ru>
2023-10-24 13:53:33 +0300

    enter data LK

--- src/test/java/LogInOTUS.java
+++ src/test/java/LogInOTUS.java
@@ -1,9 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -40,9 +40,12 @@ public class LogInOTUS {
     @Test
     public void loginOtus() {
 
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
 //   1. Открыть https://otus.ru
         driver.get("https://otus.ru");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));//ждем окончания загрузки сайта
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));//ждем окончания загрузки сайта
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(), 'Войти')]")));
 //   2. Авторизоваться на сайте
         driver.findElement(By.xpath("//button[contains(text(), 'Войти')]")).click();//нажать кнопку войти
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath
@@ -50,23 +53,58 @@ public class LogInOTUS {
         //ввести логин пароль
 
         driver.findElement(By.xpath("//div[contains(text(), 'Войдите в свой аккаунт')]/..//label[contains(text(),'Электронная почта')]/..")).click();
-//        driver.findElement(By.cssSelector("//input[@name='email']")).sendKeys(email);//login "dafome4086@aicogz.com"
-        driver.findElement(By.cssSelector("//input[@name='email']")).sendKeys("dafome4086@aicogz.com");//login "dafome4086@aicogz.com"
+//        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);//login "dafome4086@aicogz.com"
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("dafome4086@aicogz.com");//login "dafome4086@aicogz.com"
         driver.findElement(By.xpath("//div[contains(text(), 'Войдите в свой аккаунт')]/..//label[contains(text(),'Пароль')]/..")).click();
-//        driver.findElement(By.cssSelector("//input[@type='password']")).sendKeys(password);//pwd "OtusTest12#"
-        driver.findElement(By.cssSelector("//input[@type='password']")).sendKeys("OtusTest12#");//pwd "OtusTest12#"
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='password']")));
+//        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);//pwd "OtusTest12#"
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("OtusTest12#");//pwd "OtusTest12#"
         driver.findElement(By.xpath("//div[contains(text(), 'Войти')]"))
                 .click();//нажать войти
         //проверяем, что вход успешный
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("//img[@src='/_next/static/images/img/pink-owl-8e5a051ea32951df20c17b6479d3677f.png']")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("img[src*='owl-']")));
 //   3. Войти в личный кабинет
         driver.get("https://otus.ru/learning");
+        //проверяем, что перешли в личный кабинет
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h1.title__text")));
 //    В разделе "О себе" заполнить все поля "Личные данные" и добавить не менее двух контактов
+        //перейти в раздел "Осебе"
+        driver.get("https://otus.ru/lk/biography/personal/");
+        //проверяем, что перешли в раздел Осебе
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h3.text.text_h2.text_pad")));
+        //Заполняем все поля "Личные данные"
+        clearAndEnter(By.id("id_fname"),"Тест");
+        clearAndEnter(By.id("id_lname"),"Тестов");
+        clearAndEnter(By.id("id_blog_name"),"Test Testoff");
+        clearAndEnter(By.cssSelector("input[name='date_of_birth']"),"12.10.2003");
+        clearAndEnter(By.id("id_fname_latin"),"Test");
+        clearAndEnter(By.id("id_lname_latin"),"Testoff");
+
+        driver.findElement(By.cssSelector("div[data-ajax-slave='/lk/biography/cv/lookup/cities/by_country/']")).click();
+        wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.cssSelector(".lk-cv-block__select-scroll.lk-cv-block__select-scroll_country.js-custom-select-options")));
+        js.executeScript("arguments[0].scrollIntoView();", driver.findElement(By.xpath("//p[contains(text(), 'Контактная информация')]")));
+        driver.findElement(By.cssSelector(".lk-cv-block__select-scroll.lk-cv-block__select-scroll_country.js-custom-select-options>button[data-value='2']")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[data-title='Город']")));
+        driver.findElement(By.cssSelector("input[data-title='Город']")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[data-empty='Город']")));
+        driver.findElement(By.cssSelector("button[title='Орша']")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[data-title='Уровень знания английского языка']")));
+        driver.findElement(By.xpath("//input[@data-title='Уровень знания английского языка']/..")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[title='Начальный уровень (Beginner)']")));
+        driver.findElement(By.cssSelector("button[title='Начальный уровень (Beginner)']")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.container__row.lk-cv-block__line.lk-cv-block__line_double")));
+        driver.findElement(By.xpath("//input[@value='True']/..")).click();
+        System.out.println("true");
 //    Нажать сохранить
 //    Открыть https://otus.ru в "чистом браузере"
 //    Авторизоваться на сайе
 //    Войти в личный кабинет
 //    Проверить, что в разделе "О себе" отображаются указанные ранее данные
+    }
+    private void clearAndEnter(By by, String text){
+        driver.findElement(by).clear();
+        driver.findElement(by).sendKeys(text);
     }
 
 }
